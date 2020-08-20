58dfa86514bcefc533356c2e2c159978389c179f
Joyce Querubino <joycequerubino5@gmail.com>
2020-08-20 20:23:35 -0300

    Atualizando

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const nunjucks = require('nunjucks')
 
 const server = express()
 
-//Adicionando os dados
+//Adicionando os dados das receitas
 const dados = require("./data")
 
 // Adicionando CSS
@@ -19,18 +19,41 @@ nunjucks.configure("views", {
 
 // Rota da página home
 server.get("/", function (req, res){
+
+    // Página de erro
+    server.use(function (req,res) {
+        res.status(404).render("not-found")
+    })
+
     return res.render("home", {items: dados})
 })
 
 //Rota página sobre
 server.get("/sobre", function (req , res){
+
     return res.render("sobre")
 })
 
-//Rota página receitas
+//Rota página de lista de receitas
 server.get("/receitas", function (req , res){
+
     return res.render("receitas", {items: dados})
 })
+
+//Rota página de receita unica
+server.get('/receita/:index', function(req, res){
+    const recipeIndex = req.params.index
+    
+    const receita = receitas[recipeIndex]
+    
+    if (recipeIndex <= (receitas.length-1) || Number.isInteger(recipeIndex)) {
+        return res.render("receita", { receita })
+    } else{
+        res.status(404).render("not-found");
+    }
+    
+})
+
 
 server.listen(5000, function() {
     console.log("server is running")
