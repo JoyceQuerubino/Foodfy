4c84427d691249db817ae71c89097f41269d0051
Joyce Querubino <joycequerubino5@gmail.com>
2021-01-13 23:47:06 -0300

    Atualizando

--- public/scripts.js
+++ public/scripts.js
@@ -1,10 +1,15 @@
-const cards = document.querySelectorAll('.card')
+const cards = document.querySelectorAll(".card");
 
 // Para cada cartão de cartões faça:
-for (let i=0; i < cards.length; i++) {
-    const recipe = cards[i]
-    recipe.addEventListener('click', () => {
-        window.location.href = `/receita/${i}`
-    })
+for (let i = 0; i < cards.length; i++) {
+  const recipe = cards[i];
+  recipe.addEventListener("click", () => {
+    window.location.href = `/receita/${i}`;
+  });
 }
 
+// Ação de mostrar e esconder
+
+const teste = document.querySelectorAll(".mostraEsconde");
+
+console.log("TESTE");
